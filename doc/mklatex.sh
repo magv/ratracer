933a73c6cc9fe19824dc377ee66c65f27c340135
Vitaly Magerya <magv@tx97.net>
2022-12-05 15:08:51 +0100

    Link \manCm{cmd} to the cmd definition in the docs

--- doc/mklatex.sh
+++ doc/mklatex.sh
@@ -4,8 +4,8 @@ echo '\begin{itemize}'
 cat |
     sed -n '{/COMMANDS/,/AUTHORS/p;/AUTHORS/d}' |
     sed -E '/AUTHORS/d;/COMMANDS/d' |
-    sed -E 's/^    (Cm\{.*)/\\item \1\n/g' |
-    sed -E 's/([A-Z][a-z]\{)/\\man\1/g' |
+    sed -E 's/^    (Cm\{.*)/\\item Def\1\n/g' |
+    sed -E 's/((Def)?[A-Z][a-z]\{)/\\man\1/g' |
     sed -E 's/--/-{}-/g' |
     sed -E 's/^  *//' |
     sed -E 's/(FireFly|Kira)/\\noun{\1}/g' |
